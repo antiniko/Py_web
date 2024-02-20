9202655f42a5570706673a25ff99f21e07bb12d9
antiniko <100082819+antiniko@users.noreply.github.com>
2024-02-20 22:49:24 +0300

    Update test_HW.py

--- Less_3/test_HW.py
+++ Less_3/test_HW.py
@@ -18,7 +18,6 @@ def test_step1(browser):
     testpage.enter_name_for_contact("bobot")
     testpage.enter_emael_for_contact("bobot@bobot.bobot")
     testpage.enter_content_for_contact("bobot was here")
-    time.sleep(1)
     testpage.click_contact_us_button()
     time.sleep(1)
     assert testpage.contact_us_alert("Form successfully submitted")
